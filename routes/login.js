23fbec776bb89769117837c332f3aaca74b0025e
Pieter-Jan Delbecke <pjdelbecke@gmail.com>
2022-11-16 09:05:29 +1100

    get logins

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/login", (req, res) => {
+	console.log("LOGIN GET");
+	res.send("GET");
+});
+
 router.post("/login", (req, res) => {
 	console.log(req.body);
 	res.send("LOGGING IN");
